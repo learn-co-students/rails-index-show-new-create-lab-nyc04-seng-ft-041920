bc5f225b1638429cbef264c1d0bc29ec94512887
Jordan Hudgens <jordan@wow.com>
2015-11-24 10:46:49 -0600

    Implemented solution

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151124163640) do
+
+  create_table "coupons", force: :cascade do |t|
+    t.string   "coupon_code"
+    t.string   "store"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
