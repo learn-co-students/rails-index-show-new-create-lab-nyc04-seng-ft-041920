ae1dad63c0ed0491757b8065d0df437e992a12c5
Jordan Hudgens <jordan@wow.com>
2015-11-24 10:46:49 -0600

    Implemented solution

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,5 @@
 class CouponsController < ApplicationController
+
   def index
     @coupons = Coupon.all
   end
@@ -7,12 +8,13 @@ class CouponsController < ApplicationController
     @coupon = Coupon.find(params[:id])
   end
 
-  def new; end
+  def new
+  end
 
   def create
     @coupon = Coupon.new
-    @coupon[:coupon_code] = params[:coupon][:coupon_code]
-    @coupon[:store] = params[:coupon][:store]
+    @coupon[:coupon_code] = params[:coupon_code]
+    @coupon[:store] = params[:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
   end
