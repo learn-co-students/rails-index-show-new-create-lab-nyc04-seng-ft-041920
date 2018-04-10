55c6ab47105a583522fa2ac95e28366384c1fb76
Andrew Cohn <andrewcohn0@gmail.com>
2018-04-10 15:18:55 -0400

    update solution to nest coupon data in form

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,4 @@
 class CouponsController < ApplicationController
-
   def index
     @coupons = Coupon.all
   end
@@ -8,13 +7,12 @@ class CouponsController < ApplicationController
     @coupon = Coupon.find(params[:id])
   end
 
-  def new
-  end
+  def new; end
 
   def create
     @coupon = Coupon.new
-    @coupon[:coupon_code] = params[:coupon_code]
-    @coupon[:store] = params[:store]
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
   end
