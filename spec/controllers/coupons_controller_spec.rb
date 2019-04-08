ecfeddf8e80fcb531a14e688d95d3dbcc820005b
maxwellbenton <maxwellbenton@gmail.com>
2019-04-08 11:19:44 -0400

    updates deprecated controller test style

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -6,12 +6,12 @@ describe CouponsController do
     context "with valid attributes" do
       it "creates a new coupon" do
         expect{
-          post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
+          post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
         }.to change(Coupon,:count).by(1)
       end
-      
+
       it "redirects to the new coupon" do
-        post :create, { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
+        post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
         expect(response).to redirect_to Coupon.last
       end
     end
