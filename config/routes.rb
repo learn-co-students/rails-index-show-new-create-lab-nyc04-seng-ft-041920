06cc9f94d4367b410243f8d787ad751fdf499bc7
jinnic <yoohyojin@gmail.com>
2020-06-06 15:37:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  # get '/index', to: 'coupons#index'
+  resources :coupons, only: [:index, :show, :new, :create]
 end
