772ad36909ae29a4d2d3e8967a70a5ecb4f88f3d
iamkevn <iamkevnrodriguez@gmail.com>
2020-06-10 19:23:18 -0400

    Done.

--- spec/controllers/coupons_controller_spec.rb
+++ spec/controllers/coupons_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
-describe CouponsController do
+RSpec.describe CouponsController, type: :controller do
 
-  describe "POST create" do
-    context "with valid attributes" do
-      it "creates a new coupon" do
-        expect{
-          post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-        }.to change(Coupon,:count).by(1)
-      end
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      it "redirects to the new coupon" do
-        post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-        expect(response).to redirect_to Coupon.last
-      end
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
     end
   end
 
---
+++ spec/factories/coupons.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :coupon do
+    coupon_code { "MyString" }
+    store { "MyString" }
+  end
+end
